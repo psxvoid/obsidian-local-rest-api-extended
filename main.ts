d5962bdc31bd26bb027141bc0ddc8654f9fc6bab
psxvoid <pavel.sapehin@gmail.com>
2024-08-03 12:54:29 +0200

    [POC] Add frontmatter POST endpoint

--- main.ts
+++ main.ts
@@ -13,7 +13,24 @@ export default class ObsidianLocalRESTAPISamplePlugin extends Plugin {
 		// 2. Add your routes -- `addRoute` returns a route object
 		//    https://www.geeksforgeeks.org/express-js-router-route-function/
 		//    that you can attach handlers to
-		this.api.addRoute("/my-route/").get((request, response) => {
+		this.api.addRoute("/frontmatter/(.*)").post(async (request, response) => {
+			const path = request.params[0];
+			const tFile = this.app.vault.getFileByPath(path)
+
+			if (request.query == null) {
+				throw new Error("Frontmatter fields to update aren't found in the request params.")
+			}
+
+			if (tFile == null) {
+				throw new Error("Unable to find the requested file.")
+			}
+
+			await this.app.fileManager.processFrontMatter(tFile, (frontmatter) => {
+				for (let [k, v] of Object.entries(request.query)) {
+					frontmatter[k] = v;
+				}
+			});
+
 			response.status(200).json({
 				sample_plugin_response_ok: true,
 			});
@@ -24,19 +41,11 @@ export default class ObsidianLocalRESTAPISamplePlugin extends Plugin {
 		// the API itself: https://github.com/coddingtonbear/obsidian-local-rest-api/blob/main/src/requestHandler.ts
 	}
 
-	//
-	//
-	//
-	//
 	// Everything below this point can be left as it is -- this is just
 	// setting up machinery to properly register your routes with
 	// Obsidian Local REST API
-	//
-	//
-	//
-	//
-
 	async onload() {
+
 		if (this.app.plugins.enabledPlugins.has("obsidian-local-rest-api")) {
 			this.registerRoutes();
 		}
@@ -50,8 +59,8 @@ export default class ObsidianLocalRESTAPISamplePlugin extends Plugin {
 	}
 
 	onunload() {
-		if (this.api) {
-			this.api.unregister();
+		if (this.api && typeof (this.api as any).unregister === 'function') {
+			(this.api as any).unregister();
 		}
 	}
 }
@@ -62,6 +71,7 @@ declare module "obsidian" {
 			enabledPlugins: Set<string>;
 		};
 	}
+
 	interface Workspace {
 		on(
 			name: "obsidian-local-rest-api:loaded",
